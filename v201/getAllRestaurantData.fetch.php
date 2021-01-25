e2abea564b1dc25886bc7753bb447fd62b00b1da
MrM <MMMohebi@outlook.com>
2021-01-25 18:59:50 +0330

    get group info each time

--- v201/getAllRestaurantData.fetch.php
+++ v201/getAllRestaurantData.fetch.php
@@ -7,11 +7,14 @@ if(strlen($_POST['englishName']) > 2){
     include_once "DataAccess/MysqldbAccess.php";
     include_once "DataAccess/db.config.php";
 
+    $connOur = MysqlConfig::connOurs();
+    $ourAccess = new MysqldbAccess($connOur);
+
     $connRes = MysqlConfig::connRes($_POST['englishName']);
     $resAccess = new MysqldbAccess($connRes);
 
 
-    $foodListArr = getFoodList($resAccess);
+    $foodListArr = getFoodList($resAccess,$connOur);
     $infoListArr = getResInfo($resAccess);
 
 
@@ -33,13 +36,29 @@ if(strlen($_POST['englishName']) > 2){
 
 
 
-function getFoodList($resAccess){
+function getFoodList($resAccess, $ourAccess){
     $foodsList = $resAccess->select("*", "foods", false,"`foods_id` DESC");
+    $groupsInfo = $ourAccess->select("*", "food_group");
     for($i = 0; $i < count($foodsList) ; $i++){
         foreach ($foodsList[$i] as $key => $val){
             $foodsList[$i][$key] = is_numeric($val) ? $val+0: $val;
         }
-        $foodsList[$i]['group'] = json_decode($foodsList[$i]['group']);
+        $group=array();
+        foreach ($groupsInfo as $eGroup){
+            if($eGroup["englishName"] == $foodsList[$i]['group'])
+                $group = $eGroup;
+        }
+
+        $groupInfo = array(
+            "englishName"=>$group['english_name'],
+            "persianName"=>$group['persian_name'],
+            "logo"=>$group['logo'],
+            "status"=>$group['status'],
+            "rank"=>$group['rank'],
+            "averageColor"=>$group['average_color'],
+            "type"=>$group['type'],
+        );
+        $foodsList[$i]['group'] = $groupInfo;
         $foodsList[$i]['details'] = json_decode($foodsList[$i]['details']);
         $foodsList[$i]['photos'] = json_decode($foodsList[$i]['photos']);
         $foodsList[$i]['related_price_range'] = json_decode($foodsList[$i]['related_price_range']);
