d391503ba66120e5a1f6ecb34e684014bccee1bf
MrM <MMMohebi@outlook.com>
2021-02-04 17:02:16 +0330

    add db access

---
+++ v201/test/T1.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "../DataAccess/MysqldbAccess.php";
+include_once "../DataAccess/db.config.php";
+$connOurs = MysqlConfig::connOurs();
+$oursAccess = new MysqldbAccess($connOurs);
+
+$connRes = MysqlConfig::connRes("cuki");
+$resAccess = new MysqldbAccess($connRes);
+
