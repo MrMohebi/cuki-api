50e37d451cff67a4285f70ed2f26354528c25aa5
MrM <MMMohebi@outlook.com>
2021-02-04 18:21:47 +0330

    add getOpenOrders.fetch.php

---
+++ v201/getOpenOrders.fetch.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+
+if(isset($_POST['token'])){
+    include_once "DataAccess/MysqldbAccess.php";
+    include_once "DataAccess/db.config.php";
+
+    $connOurs = MysqlConfig::connOurs();
+    $oursAccess = new MysqldbAccess($connOurs);
+
+    // is token valid and has access
+    if(!($oursAccess->isTokenValid($_POST['token'], "ours_customers"))){
+        exit(json_encode(array('statusCode'=>401, "details"=>"token is not valid")));
+    }
+
+    $englishName =  mysqli_real_escape_string($connOurs, $_POST['englishName']);
+
+    $connRes = MysqlConfig::connRes($englishName);
+    $resAccess = new MysqldbAccess($connRes);
+
+    $openOrdersList = $resAccess->select("*", "orders", "`order_status`!='deleted' AND `order_status`!='done'");
+
+    if(sizeof($openOrdersList) > 0){
+        exit(json_encode(array('statusCode'=>200, 'data'=>$openOrdersList)));
+    }else{
+        exit(json_encode(array('statusCode'=>404, "details"=>"nothing found!")));
+    }
+
+}else{
+    exit(json_encode(array('statusCode'=>400, 'details'=>"wrong inputs")));
+}
