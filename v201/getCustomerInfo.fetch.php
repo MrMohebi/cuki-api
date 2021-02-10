dd2593471f7fa739533de728791e954993988ca9
MrM <MMMohebi@outlook.com>
2021-02-10 17:48:51 +0330

    get orders info

--- v201/getCustomerInfo.fetch.php
+++ v201/getCustomerInfo.fetch.php
@@ -24,13 +24,15 @@ if(isset($_POST['token'])){
     $userPhone = $oursAccess->select("phone", "ours_customers", "`token`='$token'");
     $customerInfo = $resAccess->select("*", "restaurant_customers","`phone`='$userPhone'");
 
+    $ordersListInfo = $resAccess->select('*', "orders", "`customer_phone`='$userPhone'","`ordered_date` DESC LIMIT 40");
+
     if(sizeof($customerInfo) > 3){
         $customerInfo_arranged = array(
             'phone'=> $customerInfo['phone'],
             'totalBought'=> $customerInfo['total_price'],
             'orderTimes'=> $customerInfo['order_times'],
             'score'=> $customerInfo['score'],
-            'orderList'=> $customerInfo['order_list'],
+            'orderList'=> $ordersListInfo,
             'rank'=> $customerInfo['rank'],
             'lastOrderDate'=> $customerInfo['modified_date'],
         );
@@ -41,4 +43,11 @@ if(isset($_POST['token'])){
 
 }else{
     exit(json_encode(array('statusCode'=>400, 'details'=>"wrong inputs")));
+}
+
+
+function characterFixer($str){
+    return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+        return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+    }, $str);
 }
