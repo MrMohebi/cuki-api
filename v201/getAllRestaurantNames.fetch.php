acb1da28ab632e4e0fb40ec340236263b1ebb23e
MrMohebi <MMMohebi@outlook.com>
2021-03-11 15:52:13 +0330

    add getAllRestaurantNames.fetch.php v201

---
+++ v201/getAllRestaurantNames.fetch.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+
+include_once "DataAccess/MysqldbAccess.php";
+include_once "DataAccess/db.config.php";
+
+$connOur = MysqlConfig::connOurs();
+$ourAccess = new MysqldbAccess($connOur);
+
+$restaurants = $ourAccess->select("*", "restaurants", "`position`='admin'");
+$restaurants = isset($restaurants['restaurants_id']) ? array($restaurants) : $restaurants;
+
+$result = array();
+foreach ($restaurants as $eachRes) {
+    array_push($result, array(
+        "restaurantId"=>$eachRes['restaurants_id'],
+        "englishName" => $eachRes['english_name'],
+        "persianName" => $eachRes['persian_name'],
+    ));
+}
+
+exit(json_encode(array('statusCode'=>200, "data"=>$result)));
